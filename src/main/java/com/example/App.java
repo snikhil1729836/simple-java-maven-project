ae2a9145c1e3d7fc19809cef3b85bd208f3cf940
snikhil1729836 <shakalinikhil@gmail.com>
2026-04-20 16:35:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,20 @@
-
 package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("======================================");
-        System.out.println("   Hello from Simple Java Maven App   ");
-        System.out.println("======================================");
-        Thread.sleep(Long.MAX_VALUE);
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Simple Java Maven App";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
